5a33d33f1f60788afb22db01819077c92b8557f8
windyka <officialwindyka@gmail.com>
2019-01-01 03:29:44 +0700

    10.Operators Presedence

--- index.js
+++ index.js
@@ -1,30 +1,5 @@
-// 1 = 00000001
-// 2 = 00000010
-// R OR = 00000011 // = 3
-// R AND = 00000000 // = 0
+// let x = 2 + 3 * 5;
+// console.log(x); //return 17
 
-// console.log(1 | 2); //Bitwise OR // return 3
-// console.log(1 & 2); //Bitwise AND //return 0
-
-// Read, Write, Execute
-// 00000100
-// 00000010
-// 00000001
-
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
-
-// let myPermission = 0;
-// myPermission = myPermission | readPermission | writePermission;
-// console.log(myPermission); //return 6
-
-// let message = myPermission & readPermission ? 'yes' : 'no';
-// console.log(message); //return 'yes'
-
-let myPermission = 0;
-myPermission = myPermission | writePermission;
-console.log(myPermission); //return 2
-
-let message = myPermission & readPermission ? 'yes' : 'no';
-console.log(message); //return 'no'
+let x = (2 + 3) * 5;
+console.log(x); //return 25
