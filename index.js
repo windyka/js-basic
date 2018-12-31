ed367d89fbd4a0f324cccbcf7c66ca3bedcbe037
windyka <officialwindyka@gmail.com>
2018-12-31 23:05:30 +0700

    3.Assignment Operators

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 // Arithmethic Operators
 let x = 10;
-let y = 3;
 
-console.log(x + y); //addition
-console.log(x - y); //subtraction
-console.log(x * y); //multiplication
-console.log(x / y); //division
-console.log(x % y); //remainder of division ( modulus operators )
-console.log(x ** y); //exponentation ( x power of y )
+x++; //same as
+x = x + 1;
 
-//Increment (++)
-// console.log(++x); //11
+x = x + 5; //same as
+x += 5;
 
-// console.log(x++); //10
-// console.log(x); //11
+x = x * 5; //same as
+x *= 5;
 
-//decrement (--)
-console.log(--x); //9
+x = x / 5; // same as
+x /= 5;
+//etc
