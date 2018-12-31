35263076c61949ef67958a1967b9116b7c16bb29
windyka <officialwindyka@gmail.com>
2018-12-31 22:55:21 +0700

    increment, decrement, addition, subtraction, multiplication, division remainder ov division(modulus), exponentation( x poer of y)

--- index.js
+++ index.js
@@ -2,9 +2,18 @@
 let x = 10;
 let y = 3;
 
-console.log(x + y);
-console.log(x - y);
-console.log(x * y);
-console.log(x / y);
-console.log(x % y);
-console.log(x ** y);
+console.log(x + y); //addition
+console.log(x - y); //subtraction
+console.log(x * y); //multiplication
+console.log(x / y); //division
+console.log(x % y); //remainder of division ( modulus operators )
+console.log(x ** y); //exponentation ( x power of y )
+
+//Increment (++)
+// console.log(++x); //11
+
+// console.log(x++); //10
+// console.log(x); //11
+
+//decrement (--)
+console.log(--x); //9
