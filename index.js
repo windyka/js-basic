d97029c9490edd506b8e078c8917042960a106a2
windyka <officialwindyka@gmail.com>
2018-12-31 20:02:20 +0700

    using Bracket Notation II

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ person.name = 'John';
 //using Bracket Notation
 person['name'] = 'Marry';
 
+//using Bracket Notation II
+let selection = 'name';
+person[selection] = 'Marry';
+
 console.log(person.name);
