a38b1c215826024788ff73ddcc1a50c3800d3c28
windyka <officialwindyka@gmail.com>
2019-01-01 04:04:15 +0700

    Control Flow 1.If...else

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-let a = 10;
-let b = 20;
+// Hour
+//If Hour is between 6am and 12pm: Good Morning!
+// If it's between 12pm and 6pm: Good Afternoon!
+// Otherwise: Good Evening!
 
-let c = a;
-a = b;
-b = c;
+let hour = 10;
 
-console.log(a); //return 20
-console.log(b); //return 10
+if (hour >= 6 && hour < 12) console.log('Good Morning!');
+else if (hour >= 12 && hour < 18) console.log('Good Afternoon!');
+else console.log('Good Evening!');
