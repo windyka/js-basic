012be362c654fbd064f34a76ad653354e92e6ef9
windyka <officialwindyka@gmail.com>
2018-12-31 22:19:41 +0700

    8.Types of Functions : Performing a task, Calculating a value

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+//Performing a Task
 function greet(name, lastName) {
   console.log('Hello ' + name + ' ' + lastName + '!');
 }
+
+//Calculating a value
+function square(number) {
+  return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
+console.log(square(2));
 
 greet('John', 'Smith');
