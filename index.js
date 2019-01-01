dd31616a62fede28a6dc969b09d0d1e2d839fcce
windyka <officialwindyka@gmail.com>
2019-01-01 23:22:18 +0700

    12- Exercise 3- FizzBuzz ( Correction my earlier Commit )

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-// const output = fizzBuzz(3);
-// console.log(output);
+const output = fizzBuzz('Joy');
+console.log(output);
 
-// function fizzBuzz(input) {
-//   // console.log(input);
-// }
+function fizzBuzz(input) {
+  if (typeof input !== 'number') return NaN;
 
-//not yet work in my code
+  if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz';
+
+  if (input % 3 === 0) return 'Fizz';
+
+  if (input % 5 === 0) return 'Buzz';
+
+  return input;
+}
