b0cfe5715c37d76963d2c53f6b7c6b1e71c18d62
windyka <officialwindyka@gmail.com>
2019-01-02 02:38:20 +0700

    14- Exercise 5- Even and Odd Numbers

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-// speed limit = 70
-// 5 => 1 point
-// Math.floor(1.3)
-// 12 points -> suspended
-
-checkSpeed(75);
-
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-
-  if (speed < speedLimit + kmPerPoint) {
-    console.log('Ok');
-    return;
+function showNumber(limit) {
+  for (let i = 0; i <= limit; i++) {
+    if (i % 2 === 0) console.log(i, 'Even');
+    else console.log(i, 'Odd');
   }
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log('License Suspended');
-  else console.log('Points', points);
 }
+
+showNumber(10);
+// return ;
+// 0 "Even"
+// 1 "Odd"
+// 2 "Even"
+// 3 "Odd"
+// 4 "Even"
+// 5 "Odd"
+// 6 "Even"
+// 7 "Odd"
+// 8 "Even"
+// 9 "Odd"
+// 10 "Even"
+
+function showNumber(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? 'EVEN' : 'ODD';
+    console.log(i, message);
+  }
+}
+
+// return ;
+
+// 0 "EVEN"
+// 1 "ODD"
+// 2 "EVEN"
+// 3 "ODD"
+// 4 "EVEN"
+// 5 "ODD"
+// 6 "EVEN"
+// 7 "ODD"
+// 8 "EVEN"
+// 9 "ODD"
+// 10 "EVEN"
