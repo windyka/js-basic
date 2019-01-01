1ac67f2e74902295532dea83254e2e70663c1422
windyka <officialwindyka@gmail.com>
2019-01-02 02:52:25 +0700

    15- Exercise 6- Count Truthy

--- index.js
+++ index.js
@@ -1,41 +1,11 @@
-function showNumber(limit) {
-  for (let i = 0; i <= limit; i++) {
-    if (i % 2 === 0) console.log(i, 'Even');
-    else console.log(i, 'Odd');
-  }
+const array = [0, null, undefined, '', 1, 2, 3];
+
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
 }
 
-showNumber(10);
-// return ;
-// 0 "Even"
-// 1 "Odd"
-// 2 "Even"
-// 3 "Odd"
-// 4 "Even"
-// 5 "Odd"
-// 6 "Even"
-// 7 "Odd"
-// 8 "Even"
-// 9 "Odd"
-// 10 "Even"
-
-function showNumber(limit) {
-  for (let i = 0; i <= limit; i++) {
-    const message = i % 2 === 0 ? 'EVEN' : 'ODD';
-    console.log(i, message);
-  }
-}
-
-// return ;
-
-// 0 "EVEN"
-// 1 "ODD"
-// 2 "EVEN"
-// 3 "ODD"
-// 4 "EVEN"
-// 5 "ODD"
-// 6 "EVEN"
-// 7 "ODD"
-// 8 "EVEN"
-// 9 "ODD"
-// 10 "EVEN"
+// return 3
