fe91ca7308791ba62598a4d76f4077f5c5e08243
windyka <officialwindyka@gmail.com>
2019-01-01 15:38:09 +0700

    6.Infinite Loops

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let i = 0;
-do {
-  if (i % 2 != 0) console.log(i); //return 1, 3, 5
-  i++;
-} while (i <= 5);
+{
+  while (i < 5) i++; //if you comment this (i++) you will got infinitely loops
+  console.log(i);
+}
 
-//how if the conditions is false statement?
-let x = 9;
+console.log('-------another logging------');
+
+let x = 0;
 do {
-  if (x % 2 != 0) console.log(x); //return 9 //only this executed
-  x++; //not executed
-} while (x <= 5); //false statement never executed
+  x++; //if you comment this (i++) you will got infinitely loops
+  console.log(x);
+} while (x < 5);
+
+// for (let i = 0 ; i > 0; i++)//this output return to infinitely loops
+
+// for (let i = 0; i < 10; )//this output return to infinitely loops
