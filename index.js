57dcf8ca6cb94cd7504cbf0ffccfe893d3e8a0dd
windyka <officialwindyka@gmail.com>
2019-01-01 16:13:51 +0700

    8. For...Of Loop

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ const person = {
 for (let key in person) console.log(key, person[key]);
 // Return -> name Wind
 //        -> age 35
+
+console.log('------Line logging Separator------');
+
+let colors = ['red', 'green', 'blue'];
+for (let color of colors) console.log(color);
