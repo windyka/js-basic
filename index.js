e809edcb347d2a938b6781592cd24571f8d3b23e
windyka <officialwindyka@gmail.com>
2019-01-02 23:27:38 +0700

    19- Exercise 10- Stars

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const marks = [80, 80, 50];
+showStar(10);
 
-// 1-59 : F
-// 60-69 : D
-// 70-79 : C
-// 80-89 : B
-// 90-100 : A
-
-console.log(calculateGrade(marks));
-
-function calculateGrade(marks) {
-  const average = calculateAverage(marks);
-  let sum = 0;
-
-  if (average < 60) return 'F';
-  if (average < 70) return 'D';
-  if (average < 80) return 'C';
-  if (average < 90) return 'B';
-  return 'A';
+function showStar(rows) {
+  for (let row = 0; row < rows; row++) {
+    let pattern = '';
+    for (let i = 0; i < row; i++) pattern += '*';
+    console.log(pattern);
+  }
 }
 
-function calculateAverage(array) {
-  let sum = 0;
-  for (let value of array) sum += value;
-  return sum / array.length;
-}
+// return
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
