9ab58ee210071a28f3d00700109ed970401c995e
windyka <officialwindyka@gmail.com>
2019-01-02 23:43:42 +0700

    20- Exercise- Prime Numbers

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-showStar(10);
-
-function showStar(rows) {
-  for (let row = 0; row < rows; row++) {
-    let pattern = '';
-    for (let i = 0; i < row; i++) pattern += '*';
-    console.log(pattern);
-  }
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number)) console.log(number);
 }
 
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+
+  return true;
+}
+
+showPrimes(20);
 // return
-// *
-// **
-// ***
-// ****
-// *****
-// ******
-// *******
-// ********
-// *********
+// 2
+// 3
+// 5
+// 7
+// 11
+// 13
+// 17
+// 19
