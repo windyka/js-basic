3096b701fb00b30b917cac48d842c7ba38e99bb2
windyka <officialwindyka@gmail.com>
2019-01-06 19:09:11 +0700

    15- Exercise 1- Address Object

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-// Javascript Date
+// Address Object
 
-const now = new Date();
-const date1 = new Date('Jan 6 2019 18:06');
-const date2 = new Date(2019, 0, 19, 7);
+let address = {
+  street: 'a',
+  city: 'b',
+  zipCode: 'c'
+};
 
-// write to console
-// date1.toDateString() // output:
-// "Sun Jan 06 2019"
-// date2.toTimeString() // output:
-// "07:00:00 GMT+0700 (Western Indonesia Time)"
+function showAddress(address) {
+  for (let key in address) console.log(key, address[key]);
+}
+
+showAddress(address);
+// output
+// street a
+// city b
+// zipCode c
