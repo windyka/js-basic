7bfdd705220c025b70767c1c14940d9574041367
windyka <officialwindyka@gmail.com>
2019-01-06 17:54:16 +0700

    13- Template Literals

--- index.js
+++ index.js
@@ -1,67 +1,23 @@
-// String
+// Using Template Literal (ES6)
+// Using Back Tick (``) Character
 
-// String Primitives
-const message1 = 'my';
-// typeof message
-// "string"
+const name = 'John';
+const message = `Hi ${name} ${5 + 7}
 
-// String Object
-const another1 = new String('my');
-// typeof another
-// "object"
+This is my First message
 
-const message = 'This is my first message';
-// type in console:
-// message.length
-// 24
-// message[0]
-// "T"
-// message[1]
-// "h"
-// message.includes('my')
-// true
-// message.includes('not')
-// false
-// message.startsWith('This')
-// true
-// message.startsWith('this')
-// false
-// message.endsWith('ge')
-// true
-// message.endsWith('o')
-// false
-// message.indexOf('my')
-// 8
-// message.replace('my', 'your')
-// "This is your first message"
-// message.toUpperCase() 
-// "THIS IS MY FIRST MESSAGE"
-// message.split(' ')
-// (5) ["This", "is", "my", "first", "message"]
-// 0: "This"
-// 1: "is"
-// 2: "my"
-// 3: "first"
-// 4: "message"
-// length: 5
-// __proto__: Array(0)
+I hope you Enjoy these days!
 
+Regards
+'Wind'`;
 
-​
+console.log(message);
+// so will output
+// Hi John 12
 
+// This is my First message
 
-// ----------------------- String Escape --------------------- //
+// I hope you Enjoy these days!
 
-// \0	        karakter NUL
-// \'	        petik satu
-// \"	        petik dua
-// \\	        garis miring
-// \n	        baris baru
-// \r	        carriage return
-// \v	        tab vertikal
-// \t	        tab
-// \b	        backspace
-// \f	        form feed
-// \uXXXX	    unicode codepoint
-// \u{X} ...    \u{XXXXXX}	unicode codepoint
-// \xXX	        karakter Latin-1
+// Regards
+// 'Wind'
