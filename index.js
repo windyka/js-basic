138ec56d8dc779b7a565f9ee9ea0b7a68a458755
windyka <officialwindyka@gmail.com>
2019-01-06 14:32:00 +0700

    12- String

--- index.js
+++ index.js
@@ -1,137 +1,67 @@
-// Properties
-// Math.E
-// Euler's constant and the base of natural logarithms, approximately 2.718.
+// String
 
-// Math.LN2
-// Natural logarithm of 2, approximately 0.693.
+// String Primitives
+const message1 = 'my';
+// typeof message
+// "string"
 
-// Math.LN10
-// Natural logarithm of 10, approximately 2.303.
+// String Object
+const another1 = new String('my');
+// typeof another
+// "object"
 
-// Math.LOG2E
-// Base 2 logarithm of E, approximately 1.443.
+const message = 'This is my first message';
+// type in console:
+// message.length
+// 24
+// message[0]
+// "T"
+// message[1]
+// "h"
+// message.includes('my')
+// true
+// message.includes('not')
+// false
+// message.startsWith('This')
+// true
+// message.startsWith('this')
+// false
+// message.endsWith('ge')
+// true
+// message.endsWith('o')
+// false
+// message.indexOf('my')
+// 8
+// message.replace('my', 'your')
+// "This is your first message"
+// message.toUpperCase() 
+// "THIS IS MY FIRST MESSAGE"
+// message.split(' ')
+// (5) ["This", "is", "my", "first", "message"]
+// 0: "This"
+// 1: "is"
+// 2: "my"
+// 3: "first"
+// 4: "message"
+// length: 5
+// __proto__: Array(0)
 
-// Math.LOG10E
-// Base 10 logarithm of E, approximately 0.434.
 
-// Math.PI
-// Ratio of the circumference of a circle to its diameter, approximately 3.14159.
+​
 
-// Math.SQRT1_2
-// Square root of 1/2; equivalently, 1 over the square root of 2, approximately 0.707.
 
-// Math.SQRT2
-// Square root of 2, approximately 1.414.
+// ----------------------- String Escape --------------------- //
 
-// Method
-// Note that the trigonometric functions (sin(), cos(), tan(), asin(), acos(), atan(), atan2()) expect or return angles in radians. To convert radians to degrees, divide by (Math.PI / 180), and multiply by this to convert the other way.
-
-// Note that a lot of the math functions have a precision that's implementation-dependent. This means that different browsers can give a different result, and even the same JS engine on a different OS or architecture can give different results.
-
-// Math.abs(x)
-// Returns the absolute value of a number.
-
-// Math.acos(x)
-// Returns the arccosine of a number.
-
-// Math.acosh(x)
-// Returns the hyperbolic arccosine of a number.
-
-// Math.asin(x)
-// Returns the arcsine of a number.
-
-// Math.asinh(x)
-// Returns the hyperbolic arcsine of a number.
-
-// Math.atan(x)
-// Returns the arctangent of a number.
-
-// Math.atanh(x)
-// Returns the hyperbolic arctangent of a number.
-
-// Math.atan2(y, x)
-// Returns the arctangent of the quotient of its arguments.
-
-// Math.cbrt(x)
-// Returns the cube root of a number.
-
-// Math.ceil(x)
-// Returns the smallest integer greater than or equal to a number.
-
-// Math.clz32(x)
-// Returns the number of leading zeroes of a 32-bit integer.
-
-// Math.cos(x)
-// Returns the cosine of a number.
-
-// Math.cosh(x)
-// Returns the hyperbolic cosine of a number.
-
-// Math.exp(x)
-// Returns Ex, where x is the argument, and E is Euler's constant (2.718…), the base of the natural logarithm.
-
-// Math.expm1(x)
-// Returns subtracting 1 from exp(x).
-
-// Math.floor(x)
-// Returns the largest integer less than or equal to a number.
-
-// Math.fround(x)
-// Returns the nearest single precision float representation of a number.
-
-// Math.hypot([x[, y[, …]]])
-// Returns the square root of the sum of squares of its arguments.
-
-// Math.imul(x, y)
-// Returns the result of a 32-bit integer multiplication.
-
-// Math.log(x)
-// Returns the natural logarithm (loge, also ln) of a number.
-
-// Math.log1p(x)
-// Returns the natural logarithm of 1 + x (loge, also ln) of a number.
-
-// Math.log10(x)
-// Returns the base 10 logarithm of a number.
-
-// Math.log2(x)
-// Returns the base 2 logarithm of a number.
-
-// Math.max([x[, y[, …]]])
-// Returns the largest of zero or more numbers.
-
-// Math.min([x[, y[, …]]])
-// Returns the smallest of zero or more numbers.
-
-// Math.pow(x, y)
-// Returns base to the exponent power, that is, baseexponent.
-
-// Math.random()
-// Returns a pseudo-random number between 0 and 1.
-
-// Math.round(x)
-// Returns the value of a number rounded to the nearest integer.
-
-// Math.sign(x)
-// Returns the sign of the x, indicating whether x is positive, negative or zero.
-
-// Math.sin(x)
-// Returns the sine of a number.
-
-// Math.sinh(x)
-// Returns the hyperbolic sine of a number.
-
-// Math.sqrt(x)
-// Returns the positive square root of a number.
-
-// Math.tan(x)
-// Returns the tangent of a number.
-
-// Math.tanh(x)
-// Returns the hyperbolic tangent of a number.
-
-// Math.toSource()
-// Returns the string "Math".
-
-// Math.trunc(x)
-// Returns the integral part of the number x, removing any fractional digits.
+// \0	        karakter NUL
+// \'	        petik satu
+// \"	        petik dua
+// \\	        garis miring
+// \n	        baris baru
+// \r	        carriage return
+// \v	        tab vertikal
+// \t	        tab
+// \b	        backspace
+// \f	        form feed
+// \uXXXX	    unicode codepoint
+// \u{X} ...    \u{XXXXXX}	unicode codepoint
+// \xXX	        karakter Latin-1
