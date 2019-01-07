41dfb54e2f994c12d1fe64638f33ca72c6b4a9ac
windyka <officialwindyka@gmail.com>
2019-01-07 18:19:07 +0700

    Arrays

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-let priceRanges = [
-  { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
-  { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
-  { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 30 }
-];
-
-let restaurants = [{ averagePerPerson: 5 }];
+//---------------ARRAY-----------------
