2d1ec63a1c1bd765c00347535effd33582be47d3
windyka <officialwindyka@gmail.com>
2019-01-07 21:50:16 +0700

    5- Arrow Functions

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
+// --------------Using Arrow Function---------//
 const courses = [{ id: 1, name: 'a' }, { id: 2, name: 'b' }];
 
-// const course = courses.find(function(course) {
-//   return course.name === 'b';
-// });
-
-// console.log(course);
-// // output
-// // {id: 2, name: "b"}
-
-// // ------------findIndex(function(element){...})------
-// const course = courses.findIndex(function(course) {
-//   return course.name === 'bcx';
-// });
-
-// console.log(course);
-// // output
-// // -1
-
-const course = courses.findIndex(function(course) {
-  return course.name === 'b';
-});
+const course = courses.findIndex(course => course.name === 'b');
 
 console.log(course);
 // output
