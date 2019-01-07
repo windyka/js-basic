ed2e61af59d7f77f56ef9c8968e4f8740750511f
windyka <officialwindyka@gmail.com>
2019-01-07 19:55:16 +0700

    3- Finding Elements (Primitives)

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// Adding new element of array
-const number = [3, 4];
+const number = [1, 2, 3, 1, 4];
 
-// from end
-number.push(5, 6);
-console.log(number);
-
-// from beginning
-number.unshift(1, 2);
-console.log(number);
-
-// from middle
-number.splice(2, 0, 'a', 'b');
-console.log(number);
-
+console.log(number.indexOf(1, 2));
+console.log(number.lastIndexOf(1));
+console.log(number.indexOf(1) !== -1);
+console.log(number.includes(1));
 // output
-// (4) [3, 4, 5, 6]
-// (6) [1, 2, 3, 4, 5, 6]
-// (8) [1, 2, "a", "b", 3, 4, 5, 6]
+// 3
+// 3
+// true
+// true
