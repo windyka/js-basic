81ad1bc0903bb3166e1ae5f77d1c643be490bb26
windyka <officialwindyka@gmail.com>
2019-01-07 23:22:03 +0700

    8- Combining and Slicing Arrays

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-// --------------Emptying an Array---------//
-let numbers = [1, 2, 3, 4];
-let another = numbers;
+// Combine and Slicing Arrays
+const first = [1, 2, 3, 4];
+const second = [5, 6, 7, 8];
 
-// // Solution 1 ----------------
-// numbers = [];
-
-// console.log(numbers);
-// // Output
-// // []
-
-// Solution 2 ----------------------
-numbers.length = 0;
-
-// console.log(numbers);
-// console.log(another);
-// // Output
-// // []
-// // []
-
-// // Solution 3 ----------------
-// numbers.splice(0, numbers.length);
-// console.log(numbers);
-// console.log(another);
-// // Output
-// // []
-// // []
-
-// Solution 4 ----------------------
-while (numbers.length > 0) numbers.pop();
-console.log(numbers);
-console.log(another);
+// Concat Method
+const combined = first.concat(second);
+console.log(combined);
 // Output
-// []
-// []
+// (8) [1, 2, 3, 4, 5, 6, 7, 8]
+
+// Slice Method
+const slice = combined.slice();
+console.log(slice);
+// Output
+// (8) [1, 2, 3, 4, 5, 6, 7, 8]
+
+// Combining type reference of Object
+const third = [{ id: 1 }];
+const fourth = [5, 6, 7, 8];
+
+const otherCombined = third.concat(fourth);
+third[0].id = 10;
+console.log(otherCombined);
+// Output
+// (5) [{…}, 5, 6, 7, 8]
+// 0: {id: 10}
+// 1: 5
+// 2: 6
+// 3: 7
+// 4: 8
+// length: 5
+// __proto__: Array(0)
