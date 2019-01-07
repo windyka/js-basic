47b49441f935cc909150cd78b531b4bb3009cef1
windyka <officialwindyka@gmail.com>
2019-01-07 19:00:41 +0700

    2- Adding Elements

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// -----INTRODUCTION ---------
+// Adding new element of array
+const number = [3, 4];
 
-// Adding new element
-// Finding Elements
-// Removing Elements
-// Splitting Arrays
-// Combining Arrays
+// from end
+number.push(5, 6);
+console.log(number);
+
+// from beginning
+number.unshift(1, 2);
+console.log(number);
+
+// from middle
+number.splice(2, 0, 'a', 'b');
+console.log(number);
+
+// output
+// (4) [3, 4, 5, 6]
+// (6) [1, 2, 3, 4, 5, 6]
+// (8) [1, 2, "a", "b", 3, 4, 5, 6]
