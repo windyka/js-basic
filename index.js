282c1b37c13d9c5b42d47d5877a1dea7408aea25
windyka <officialwindyka@gmail.com>
2019-01-07 23:31:19 +0700

    9- The Spread Operator

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-// Combine and Slicing Arrays
+// Combine and Slicing Arrays With Spread Operator
 const first = [1, 2, 3, 4];
 const second = [5, 6, 7, 8];
 
-// Concat Method
-const combined = first.concat(second);
+const combined = [...first, 'a', ...second, 'b'];
+
+const copy = [...combined];
+
 console.log(combined);
+console.log(copy);
 // Output
-// (8) [1, 2, 3, 4, 5, 6, 7, 8]
-
-// Slice Method
-const slice = combined.slice();
-console.log(slice);
-// Output
-// (8) [1, 2, 3, 4, 5, 6, 7, 8]
-
-// Combining type reference of Object
-const third = [{ id: 1 }];
-const fourth = [5, 6, 7, 8];
-
-const otherCombined = third.concat(fourth);
-third[0].id = 10;
-console.log(otherCombined);
-// Output
-// (5) [{…}, 5, 6, 7, 8]
-// 0: {id: 10}
-// 1: 5
-// 2: 6
-// 3: 7
-// 4: 8
-// length: 5
-// __proto__: Array(0)
+// (10) [1, 2, 3, 4, "a", 5, 6, 7, 8, "b"]
+// (10) [1, 2, 3, 4, "a", 5, 6, 7, 8, "b"]
