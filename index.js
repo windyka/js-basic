a9b1904531a75e8207cb75d4819269a170624295
windyka <officialwindyka@gmail.com>
2019-01-08 17:01:03 +0700

    15- Mapping an Array

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// Filtering the Element of an Array
+// Mappping an Array
 
 const numbers = [1, 2, -1, 3, 4];
 
-const filtered = numbers.filter(n => n >= 0);
+const items = numbers
+  .filter(n => n >= 0)
+  .map(n => ({ value: n }))
+  .filter(obj => obj.value > 1)
+  .map(obj => obj.value);
 
-console.log(filtered);
+console.log(items);
 // output
-//(4) [1, 2, 3, 4]
+// (3) [2, 3, 4]
+// 0: 2
+// 1: 3
+// 2: 4
+// length: 3
+// __proto__: Array(0)
