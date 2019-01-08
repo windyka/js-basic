6f369748c75f8e0d2567a238c0dd6c3d7f659ebe
windyka <officialwindyka@gmail.com>
2019-01-09 01:32:03 +0700

    20- Exercise 4- Moving an Element

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Searching Element with Includes(function(array, target))
+// Moving element
 const numbers = [1, 2, 3, 4];
 
-const output = except(numbers, [1, 2]); // false
+const output = move(numbers, 1, 2); // false
 
-console.log(output); // (2) [3, 4]
+console.log(output); // (4) [1, 3, 4, 2]
 
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element)) output.push(element);
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.log('Invalid offset');
+    return;
+  }
+
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
   return output;
 }
