15bdb17ea5f1440fa65160568711eb860d1b99d7
windyka <officialwindyka@gmail.com>
2019-01-09 23:01:19 +0700

    4- The Rest Operator

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-console.log(sum(1, 2, 3, 4, 5, 6, 9, 10)); // output => 40
-
-function sum() {
-  let total = 0;
-  for (let value of arguments) total += value;
-  return total;
+// Rest Operator
+function argue(args) {
+  console.log(args);
 }
+
+console.log(argue(1, 2, 3, 4, 5, 6, 9, 10));
+// output
+// (8) [1, 2, 3, 4, 5, 6, 9, 10]
+// 0: 1
+// 1: 2
+// 2: 3
+// 3: 4
+// 4: 5
+// 5: 6
+// 6: 9
+// 7: 10
+// length: 8
+// __proto__: Array(0)
+// -------------------------------------------------
+
+// sum an array with Reduce method
+// function sum(...arg) {
+//   return arg.reduce((a, b) => a + b);
+// }
+
+// console.log(sum(1, 2, 3, 4, 5));
+// output
+// 15
+
+// function sum(discount, ...prices) {
+//   const total = prices.reduce((a, b) => a + b);
+//   return total * (1 - discount);
+// }
+
+// console.log(sum(0.1, 20, 30));
+// // output
+// // 45
+// -----------------------------------------------
+
+function sum(discount, ...prices) {
+  const total = prices.reduce((a, b) => a + b);
+  return total * (1 - discount);
+}
+
+console.log(sum(0.1, 20, 30)); // output => 45
